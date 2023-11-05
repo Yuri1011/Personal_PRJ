8d936b011cae0534191c07a6eca70b44c29fa173
Yury Trypolski <yury1011.89@gmail.com>
2023-11-05 03:52:56 +0300

    change speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody playerRB;
-    private float speed = 10.0f;
+    private float speed = 15.0f;
     private float horizontalInput;
     private float verticalInput;
     Vector3 curreentPosition;
