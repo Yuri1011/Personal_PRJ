0c86fe3b69a9113b056d7c51814c104ced3ba2e2
Yury Trypolski <yury1011.89@gmail.com>
2023-11-05 03:04:56 +0300

    added range of objects destruction

--- Assets/Scripts/MoveDown.cs
+++ Assets/Scripts/MoveDown.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MoveDown : MonoBehaviour {
     private float speed = 5.0f;
+    private float destroyRange = -10.0f;
     Rigidbody obstacleRB;
     void Start() {
         obstacleRB = GetComponent<Rigidbody>();
@@ -12,5 +13,9 @@ public class MoveDown : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         obstacleRB.AddForce(Vector3.forward * -speed);
+
+        if(transform.position.z < destroyRange) {
+            Destroy(gameObject);
+        }
     }
 }
